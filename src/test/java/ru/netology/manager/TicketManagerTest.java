3715bf0c216a815eae8a897496d9bff5212bc2a9
Boris.Budenovsky <fpqb00@gmail.com>
2021-03-04 00:06:31 +0300

    added test with zero

--- src/test/java/ru/netology/manager/TicketManagerTest.java
+++ src/test/java/ru/netology/manager/TicketManagerTest.java
@@ -12,29 +12,48 @@ class TicketManagerTest {
     private Ticket first = new Ticket(1, 3290, "LED", "ROV", 140);
     private Ticket second = new Ticket(2, 2890, "LED", "ROV", 148);
     private Ticket third = new Ticket(3, 5800, "LED", "PRG", 120);
-    private Ticket fouth = new Ticket(1, 3000, "LED", "STO", 75);
+    private Ticket fouth = new Ticket(4, 3000, "LED", "STO", 75);
+    private Ticket fifth = new Ticket(5, 12000, "LED", "ROV", 75);
 
     @Test
-    public void shouldSortFromLedToRov(){
+    public void shouldSortFromLedToRov() {
         repository.save(first);
         repository.save(second);
         repository.save(third);
         repository.save(fouth);
+        repository.save(fifth);
+
         TicketManager manager = new TicketManager(repository);
-        Ticket[] expected = new Ticket[] {second, first};
+        Ticket[] expected = new Ticket[]{second, first, fifth};
         Ticket[] actual = manager.search("LED", "ROV");
         assertArrayEquals(actual, expected);
     }
 
     @Test
-    public void shouldSortFromLedToSto(){
+    public void shouldSortFromLedToSto() {
         repository.save(first);
         repository.save(second);
         repository.save(third);
         repository.save(fouth);
+        repository.save(fifth);
+
         TicketManager manager = new TicketManager(repository);
-        Ticket[] expected = new Ticket[] {fouth};
+        Ticket[] expected = new Ticket[]{fouth};
         Ticket[] actual = manager.search("LED", "STO");
+        assertArrayEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldWorkIfZero() {
+        repository.save(first);
+        repository.save(second);
+        repository.save(third);
+        repository.save(fouth);
+        repository.save(fifth);
+
+        TicketManager manager = new TicketManager(repository);
+        Ticket[] expected = new Ticket[]{};
+        Ticket[] actual = manager.search("LED", "DME");
         assertArrayEquals(actual, expected);
     }
 }
